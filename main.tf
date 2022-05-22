f1602f95b500f9fecb2822b7a74ca51f6bb80953
Ade Gbodimowo <agbodimowo@erpinternational.com>
2022-05-22 00:25:45 -0400

    Create new ec2

--- main.tf
+++ main.tf
@@ -34,3 +34,16 @@ resource "aws_instance" "web" {
   }
 }
 
+
+data "aws_instance" "ec2" {
+  filter {
+    name = "tag:Name"
+    values = ["HelloWorld"]
+  }
+}
+
+resource "aws_ami_from_instance" "jango_ami" {
+	
+	name = "jango_ami"
+	source_instance_id = "${data.aws_instance.ec2.id}"
+}
